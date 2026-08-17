c379782bb810a1be2618a631155eb1f3d5d2e600
Konstantin Pušac <115993315+KonstantinPusac@users.noreply.github.com>
2026-08-18 00:02:09 +0200

    Add: trapping-rain-water - submission-10

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-10.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int left = 0; int right = h.size()-1; int highest = 0;
+        // Finding highest point
+        for (int i = 0; i < h.size(); i++)
+            if (h[highest] < h[i])
+                highest = i;
+
+        // Filling with water
+        int water = 0; int start = left++; int end = right--;
+        while (true)
+        {
+            if (start == highest && end == highest)
+                return water;
+            
+            // Left side
+            if (start != highest)
+            {
+                if (h[start] > h[left])
+                    water += h[start] - h[left++];
+                else 
+                    start = left++;
+            }
+
+            // Right side
+            if (end != highest)
+            {
+                if (h[end] > h[right])
+                    water += h[end] - h[right--];
+                else
+                    end = right--;
+            }
+        }
+    }
+};
